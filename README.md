59e12b0fa19dc88aac4c7343824d0e434c4791ae
Jose Eduardo Sebas Villegas <jose.sebas@alumnos.udg.mx>
2019-02-21 10:56:18 -0600

    Primer commit - rest server

---
+++ README.md
@@ -0,0 +1,14 @@
+#SOCKET-SERVER
+```
+npm install para reconstruir modulos de node
+
+```
+
+```
+tsc -w Generar el dist
+```
+
+``` PARA LEVANTAR EL SERVIDOR 
+nodemon dist/
+node dist/
+```
---
+++ index.ts
@@ -0,0 +1,19 @@
+import Server from './clases/server';
+import router from './routes/router';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+const server = new Server();
+
+server.app.use(bodyParser.urlencoded({extended:true}));
+server.app.use(bodyParser.json());
+
+//cors
+server.app.use(cors({origin:true, credentials:true}));//evitar que cualquier persona haga peticiones
+
+//rutas
+server.app.use('/',router);
+
+server.start(()=>{
+	console.log('servidor corriendo en el puerto '+server.port);
+});
+
